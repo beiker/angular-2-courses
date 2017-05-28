a00a052c55a67d04104bd3a33419b0290a086823
Ivan <oscar.alcantars@gmail.com>
2017-05-28 10:37:50 -0500

    observables

--- udemy/observables-start/src/app/home/home.component.ts
+++ udemy/observables-start/src/app/home/home.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {Observer} from 'rxjs/Observer';
+import {Subscription} from 'rxjs/Subscription';
+import 'rxjs/Rx';
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    myNumbersSubscription: Subscription;
+    myObservableSubscription: Subscription;
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
 
+    ngOnInit() {
+        const myNumbers = Observable.interval(1000);
+        this.myNumbersSubscription = myNumbers.subscribe(
+            (number: number) => {
+                console.log(number);
+            }
+        );
+
+        const myObservable = Observable.create((observer: Observer<string>) => {
+            setTimeout(() => {
+                observer.next('First package');
+            }, 2000);
+            setTimeout(() => {
+                observer.next('Second package');
+            }, 4000);
+            setTimeout(() => {
+                observer.error('Error occured');
+            }, 8000);
+            setTimeout(() => {
+                observer.complete();
+            }, 6000);
+        })
+        this.myObservableSubscription = myObservable.subscribe(
+            (data: string) => {
+                console.log(data);
+            },
+            (data: string) => {
+                console.log(data);
+            },
+            () => {
+                console.log('completed');
+            },
+        );
+    }
+
+    ngOnDestroy() {
+        this.myNumbersSubscription.unsubscribe();
+        this.myObservableSubscription.unsubscribe();
+    }
 }
+
